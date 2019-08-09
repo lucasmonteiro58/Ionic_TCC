5731560fca5142272d6581ec87127f9ea1c96f0f
Lucas Monteiro <lucas.monteiro58@gmail.com>
2019-08-08 23:11:00 -0300

    botoes de baixo funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoogleMaps } from '@ionic-native/google-maps';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    GoogleMaps
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Platform, LoadingController } from '@ionic/angular';
+import { Environment, GoogleMap, GoogleMaps } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {
-    
+  @ViewChild('map', { static: true} ) public mapElement: any;
+  private loading: any;
+  private map: GoogleMap;
+
+  constructor(
+    private platform: Platform,
+    private loadingCtrl: LoadingController
+  ) {
+
+   }
+
+  ngOnInit() {
+    this.mapElement = this.mapElement.nativeElement;
+
+    this.mapElement.style.width = this.platform.width() + 'px';
+    this.mapElement.style.height = this.platform.height() + 'px';
+
+    this.loadMap();
+  }
+
+  loadMap() {
+    // this.loading = this.loadingCtrl.create({ message: 'Aguarde'});
+    // await this.loading.present();
+
+    Environment.setEnv({
+      API_KEY_FOR_BROWSER_RELEASE: 'AIzaSyCBA2sw2SDLNj41vLMlAQCi3kcWE43Bedc',
+      API_KEY_FOR_BROWSER_DEBUG: 'AIzaSyCBA2sw2SDLNj41vLMlAQCi3kcWE43Bedc'
+    });
+
+    this.map = GoogleMaps.create(this.mapElement);
   }
 
 }
